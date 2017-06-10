dcf3fa245c167ce94d287e969a85d4a60a4384a6
Vince P <ianparulan@gmail.com>
2017-06-10 15:38:48 -0400

    added map component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,20 @@
+/**
+ * Created by vinceparulan on 6/10/17.
+ */
+import React from 'react';
+
+
+class Map extends Component {
+    render() {
+        return (
+            <section>
+                <div id="map">
+                    The Map is Here
+                </div>
+            </section>
+        )
+
+    }
+}
+
+export { Map };
